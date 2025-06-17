fdb9c8dd38cdd962fd65c12321cf213ba7ecfec5
Americano5139 <kimjemomaeng@gmail.com>
2025-06-17 01:20:42 +0000

    Nextcloud retest

--- docker/config.php
+++ docker/config.php
@@ -1,16 +1,27 @@
 <?php
+$aws_key = getenv('AWS_ACCESS_KEY_ID') ?: '';
+$aws_secret = getenv('AWS_SECRET_ACCESS_KEY') ?: '';
+$rds_dbname = getenv('RDS_DB_NAME') ?: '';
+$rds_host = getenv('RDS_HOST') ?: '';
+$rds_user = getenv('RDS_USERNAME') ?: '';
+$rds_pass = getenv('RDS_PASSWORD') ?: '';
+$s3_bucket = getenv('S3_BUCKET') ?: '';
+
 $CONFIG = array (
-  'dbtype' => 'mysql',
-  'dbname' => getenv('RDS_DB_NAME'),
-  'dbhost' => getenv('RDS_HOST'),
-  'dbuser' => getenv('RDS_USERNAME'),
-  'dbpassword' => getenv('RDS_PASSWORD'),
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'datadirectory' => '/var/www/html/data',
+  'trusted_domains' => [
+    0 => 'localhost',
+    1 => '192.168.1.41',
+  ],
   'objectstore' => [
     'class' => '\\OC\\Files\\ObjectStore\\S3',
     'arguments' => [
-      'bucket' => getenv('S3_BUCKET'),
-      'key' => getenv('AWS_ACCESS_KEY_ID'),
-      'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
+      'bucket' => $s3_bucket,
+      'autocreate' => false,
+      'key'    => $aws_key,
+      'secret' => $aws_secret,
       'region' => 'ap-northeast-2',
       'use_ssl' => true,
       'use_path_style' => false,
@@ -18,6 +29,11 @@ $CONFIG = array (
       'port' => 443,
     ],
   ],
-  'installed' => false,  // 처음 배포할 땐 false 또는 제거
+  'dbtype' => 'mysql',
+  'dbname' => $rds_dbname,
+  'dbhost' => $rds_host,
+  'dbport' => '',
+  'dbuser' => $rds_user,
+  'dbpassword' => $rds_pass,
+  'installed' => false  // 초기 설치 시엔 false 또는 제거
 );
-
