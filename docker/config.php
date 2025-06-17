8455a3ea71ec6b5fb57cb0be081950cdaf4e5372
Americano5139 <kimjemomaeng@gmail.com>
2025-06-17 01:05:40 +0000

    Nextcloud page

--- docker/config.php
+++ docker/config.php
@@ -1,4 +1,23 @@
 <?php
-echo "✅ ENV RDS_HOST: " . getenv('RDS_HOST') . "\n";
-?>
+$CONFIG = array (
+  'dbtype' => 'mysql',
+  'dbname' => getenv('RDS_DB_NAME'),
+  'dbhost' => getenv('RDS_HOST'),
+  'dbuser' => getenv('RDS_USERNAME'),
+  'dbpassword' => getenv('RDS_PASSWORD'),
+  'objectstore' => [
+    'class' => '\\OC\\Files\\ObjectStore\\S3',
+    'arguments' => [
+      'bucket' => getenv('S3_BUCKET'),
+      'key' => getenv('AWS_ACCESS_KEY_ID'),
+      'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
+      'region' => 'ap-northeast-2',
+      'use_ssl' => true,
+      'use_path_style' => false,
+      'hostname' => 's3.ap-northeast-2.amazonaws.com',
+      'port' => 443,
+    ],
+  ],
+  'installed' => false,  // 처음 배포할 땐 false 또는 제거
+);
 
